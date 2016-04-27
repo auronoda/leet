aca43674fd1cd438a71cf583ee719fa29a211350
auronoda <auronoda@hotmail.com>
2016-04-27 20:51:06 +0800

    Create 50. Pow(x, n).java

---
+++ 50. Pow(x, n).java
@@ -0,0 +1,24 @@
+public class Solution {
+    public double myPow(double x, int n) {
+      if (n == 1){
+        return x;
+      }
+      else if (n == 0){
+        return 1;
+      }
+      
+      else if (n == -1){
+        return 1/x;
+      }
+      else {
+         if (n % 2 == 0){
+          double a =  myPow(x, n / 2);
+          return a * a;
+         }
+         else {
+          double a = myPow(x, (n - 1) / 2);
+          return a * a * x;
+         }
+      }
+    }
+}
