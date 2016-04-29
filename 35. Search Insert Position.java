2885b63458edbdb99f1b89331d34557c0278196b
auronoda <auronoda@hotmail.com>
2016-04-29 23:30:30 +0800

    Create 35. Search Insert Position.java

---
+++ 35. Search Insert Position.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        if (nums[start] > target){
+            return start;
+        }
+        if (nums[end] < target){
+            return end + 1;
+        }
+        while (start + 1 < end){
+            mid = (start + end) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            if (nums[mid] < target){
+                start = mid;
+            }
+            else {
+                end = mid;
+            }
+        }
+        if (nums[start] == target){
+            return start;
+        }
+        else if (nums[end] == target){
+            return end;
+        }
+        
+        return start + 1;
+    }
+}
